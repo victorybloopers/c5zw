f45ab6c63bafa39a5b16e802a82f163213756257
Michael Chase <victorybloopers@gmail.com>
2018-08-12 14:24:41 -0700

    Added drag area constraints

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,18 @@ export default {
     padding: 0;
   }
 #app {
+  position: absolute;
+  outline: 1px solid black;
+  left: 200px;
+  top: 200px;
+  width: 100px;
+  height: 100px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  z-index: 1;
+  overflow: hidden;
 }
 </style>
--- src/components/BuildingBlock.vue
+++ src/components/BuildingBlock.vue
@@ -6,7 +6,17 @@
        v-hammer:pancancel="endDrag"
        ref="someBlock"
        :class="{ 'building-block': true }"
-       :style="{ backgroundColor: 'red', left: posX + 'px', top: posY + 'px' }">{{ isDragging }}</div>
+       :style="{ backgroundColor: 'red',
+                 left: posX + 'px',
+                 top: posY + 'px',
+                 width: width + 'px',
+                 height: height + 'px'}">
+    {{ isDragging }}
+    <div class="square" style="top: 0; left: 0"></div>
+    <div class="square" style="top: 0; right: 0"></div>
+    <div class="square" style="left: 0; bottom: 0"></div>
+    <div class="square" style="right: 0; bottom: 0"></div>
+  </div>
 </template>
 
 <script>
@@ -18,8 +28,10 @@ export default {
       isDragging: false,
       lastPosX: 0,
       lastPosY: 0,
-      posX: 0,
-      posY: 0
+      posX: -50,
+      posY: -50,
+      width: 200,
+      height: 200
     }
   },
   mounted () {
@@ -37,6 +49,22 @@ export default {
 
       this.posX = e.deltaX + this.lastPosX
       this.posY = e.deltaY + this.lastPosY
+
+      if (this.posX > 0) {
+        this.posX = 0
+      }
+
+      if (this.posY > 0) {
+        this.posY = 0
+      }
+
+      if (this.posX + this.width < 100) {
+        this.posX = -100
+      }
+
+      if (this.posY + this.height < 100) {
+        this.posY = -100
+      }
 
       if (e.isFinal) {
         this.endDrag()
@@ -56,12 +84,17 @@ export default {
 </script>
 
 <style scoped>
+  .square {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    background: blue;
+  }
   .building-block {
     color: white;
     position: absolute;
-    width: 200px;
-    height: 200px;
     margin-bottom: 0.4em;
     background-color: blue;
+    z-index: -1;
   }
 </style>
